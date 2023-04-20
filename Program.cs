981b7e87b1252a329eeae008b3fc622f1d33496a
Pablo <pablogcg05@gmail.com>
2023-04-20 11:13:24 +0200

    Código definitivo

--- Program.cs
+++ Program.cs
@@ -6,30 +6,30 @@ Console.WriteLine("Introduzca el segudno número");
 var segundoNumero = Console.ReadLine();
 int numero2 = int.Parse(segundoNumero);
 Suma(numero1,numero2);
-static int Suma(int numero1, int numero2)
+static int Suma(int sumando1, int sumando2)
 {
-    int resultado = numero1 + numero2;
-    Console.WriteLine("El resultado de sumar los números " + numero1 + " y " + numero2 + " es: " + resultado);
+    int resultado = sumando1 + sumando2;
+    Console.WriteLine("El resultado de sumar los números " + sumando1 + " y " + sumando2 + " es: " + resultado);
     return resultado;
 }
 Resta(numero1,numero2);
-static int Resta(int numero1, int numero2)
+static int Resta(int minuendo, int substraendo)
 {
-    int resultado = numero1 - numero2;
-    Console.WriteLine("El resultado de restar los números " + numero1 + " y " + numero2 + " es: " + resultado);
+    int resultado = minuendo - substraendo;
+    Console.WriteLine("El resultado de restar los números " + minuendo + " y " + substraendo + " es: " + resultado);
     return resultado;
 }
 Producto(numero1,numero2);
-static int Producto(int numero1, int numero2)
+static int Producto(int factor1, int factor2)
 {
-    int resultado = numero1 * numero2;
-    Console.WriteLine("El resultado de multiplicar los números " + numero1 + " y " + numero2 + " es: " + resultado);
+    int resultado = factor1 * factor2;
+    Console.WriteLine("El resultado de multiplicar los números " + factor1 + " y " + factor2 + " es: " + resultado);
     return resultado;
 }
 Division(numero1,numero2);
-static int Division(int numero1, int numero2)
+static int Division(int dividendo, int divisor)
 {
-    int resultado = numero1 / numero2;
-    Console.WriteLine("El resultado de dividir los números " + numero1 + " y " + numero2 + " es: " + resultado);
+    int resultado = dividendo / divisor;
+    Console.WriteLine("El resultado de dividir los números " + dividendo + " y " + divisor + " es: " + resultado);
     return resultado;
 }
